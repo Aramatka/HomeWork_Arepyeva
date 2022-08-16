9e1b8d100a080b1ede513277d69a0d41a1f4d8bc
Tamara <arepyevat@gmail.com>
2022-08-16 23:45:20 +0300

    Максимум из двух

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+System.Console.WriteLine("Enter the first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Enter the second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber > secondNumber)
+{
+    System.Console.WriteLine("max=" + firstNumber);
+}
+else
+{
+       System.Console.WriteLine("max=" + secondNumber); 
+}
