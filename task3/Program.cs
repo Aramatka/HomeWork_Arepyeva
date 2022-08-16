9ea049235c46263856a8f3c0163604e8632fbcd2
Tamara <arepyevat@gmail.com>
2022-08-17 00:38:24 +0300

    Проверка четных чисел

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+System.Console.WriteLine("Enter a number to check: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if ((Number % 2) == 0)
+{
+    System.Console.WriteLine(Number + " четное");
+}
+else
+{
+        System.Console.WriteLine(Number + " нечетное");
+}
