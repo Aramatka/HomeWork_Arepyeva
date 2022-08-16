88956a75ba04382706fccc127a8a13facdf3f935
Tamara <arepyevat@gmail.com>
2022-08-17 00:47:57 +0300

    Печать четных чисел

---
+++ task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.WriteLine("Enter a number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int evenNumber = 2;
+while (evenNumber < Number)
+{
+    System.Console.Write(evenNumber + ", ");
+    evenNumber +=2;
+}
