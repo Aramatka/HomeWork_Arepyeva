48204dd747d4c610b7c91a33b285ce1427fe708c
Tamara <arepyevat@gmail.com>
2022-08-18 22:27:30 +0300

    Семинар 2, задача 10

---
+++ taskSeminar2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Нужно сделать через числа, без индексов строк.
+456 -> 5
+782 -> 8
+918 -> 1*/
+System.Console.WriteLine("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int secondDiv = (Number - (Number/100)*100)/10;
+if (Number < 100 || Number > 999)
+    {
+    System.Console.WriteLine("Просила же, трёхзначное! Поробуйте ещё раз.");
+    }
+else System.Console.WriteLine($"У {Number} вторая цифра {secondDiv}.");
