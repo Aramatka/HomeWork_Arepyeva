030a3c88078978a3b256e5ded3982baea96c1779
Tamara <arepyevat@gmail.com>
2022-08-24 23:19:56 +0300

    Семинар 4, задача 25

--- taskSeminar4_1/Program.cs
+++ taskSeminar4_1/Program.cs
@@ -2,5 +2,15 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 System.Console.WriteLine("Введите число A: ");
-
+int numberA = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int degree = 0;
+int composition = 1;
+if (numberB<0) {numberB = numberB *-1;}
+while (degree < numberB)
+{
+    composition = composition*numberA;
+    degree++;
+}
+System.Console.WriteLine(composition);
