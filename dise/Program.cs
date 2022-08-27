4c33ff1d995b0732ca0c077f9a837223f2ed6e64
Tamara <arepyevat@gmail.com>
2022-08-28 00:47:13 +0300

    Рандомайзер для дайсов

---
+++ dise/Program.cs
@@ -0,0 +1,7 @@
+﻿// Бросок дайса
+System.Console.WriteLine("Введите стоимость дайса: ");
+
+int dise = Convert.ToInt32(Console.ReadLine());
+
+int num = new Random().Next(1, dise+1);
+System.Console.WriteLine($"На кубике выпало {num}");
